0e06f6c50e7f87ed55c8509c59a7ca6fe2a4fc74
nickl- <github@jigsoft.co.za>
2018-06-08 19:27:02 +0200

    Completion hacks W.I.P.
    I discovered a little gem in bsh.util.NameCompletionTable it's the guy that does the JConsole completion.
    Gets bootstrapped with the interpreter namespace and can also work the classpath. Do try with JDK8 with rt.jar loaded for pretty neat insights, Java 9 currently can't get that detail.
    The whole MappingFeedback listener is a tad odd, without it the ClassPath echo's to stderr what it's doing.
    The rest is an attempt to dive deeper obtaining members for completion, it uses bsh.Name (nameResolver)'s magic which can be a bit like a magicians top hat, you never know what you're going to get. Good proof of concept me thinks.

--- src/main/java/ste/beanshell/ui/BshCompleter.java
+++ src/main/java/ste/beanshell/ui/BshCompleter.java
@@ -15,58 +15,90 @@
  */
 package ste.beanshell.ui;
 
-import bsh.EvalError;
+import bsh.ClassIdentifier;
+import bsh.ClassPathException;
 import bsh.Interpreter;
+import bsh.Primitive;
+import bsh.UtilEvalError;
+import bsh.classpath.BshClassPath;
+import bsh.classpath.BshClassPath.MappingFeedback;
+import bsh.classpath.ClassManagerImpl;
+import bsh.util.NameCompletionTable;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
+import java.util.stream.Stream;
+
 import org.jline.reader.Candidate;
 import org.jline.reader.Completer;
 import org.jline.reader.LineReader;
 import org.jline.reader.ParsedLine;
-import org.jline.utils.AttributedString;
 
 /**
  *
  * @author ste
  */
 public class BshCompleter implements Completer {
-    
+    static class ClassPathMappingFeedback implements MappingFeedback {
+        @Override
+        public void startClassMapping() {}
+        @Override
+        public void classMapping(String msg) {}
+        @Override
+        public void errorWhileMapping(String msg) {}
+        @Override
+        public void endClassMapping() {}
+    }
+    static {
+        BshClassPath.addMappingFeedback(new ClassPathMappingFeedback());
+    }
     private final Interpreter bsh;
+    private final NameCompletionTable names = new NameCompletionTable();
     
     /**
      * 
      * @param bsh the beanshell interpreter - NOT NULL
      */
     public BshCompleter(Interpreter bsh) {
-        if (bsh == null) {
+        if (bsh == null)
             throw new IllegalArgumentException("bsh can not be null");
-        }
         this.bsh = bsh;
+        names.add(bsh.getNameSpace());
+        if (bsh.getNameSpace().getClassManager() instanceof ClassManagerImpl) try {
+            names.add(((ClassManagerImpl) bsh.getNameSpace().getClassManager()).getClassPath());
+        } catch (ClassPathException e) {/* ignore */ }
     }
-
+    
+    Map<String,String[]> memberCache = new WeakHashMap<>();
+    private String[] findMembers(String name) {
+        if (memberCache.containsKey(name))
+            return memberCache.get(name);
+        try {
+            Object obj = Primitive.unwrap(bsh.getNameSpace().get(name, bsh));
+            if ( null != obj ) {
+                Class<?> cls = (obj instanceof Class) ? (Class<?>) obj
+                    : (obj instanceof ClassIdentifier) 
+                    ? ((ClassIdentifier) obj).getTargetClass() : obj.getClass();
+                memberCache.put(name,             
+                Stream.concat(Stream.of(cls.getFields()).map(Field::getName),
+                Stream.of(cls.getMethods()).map(Method::getName))
+                 .distinct().map((name+".")::concat).toArray(String[]::new));
+                return memberCache.get(name);
+            }
+        } catch (UtilEvalError e) {
+        }
+        return new String[0];
+        
+    }
     @Override
     public void complete(LineReader reader, ParsedLine pl, List<Candidate> candidates) {
-        try {
-            for (String v : (String[])bsh.get("this.variables")) {
-                candidates.add(new Candidate(AttributedString.stripAnsi(v), v, null, null, null, null, true));
-            }
-            
-            for (String v : (String[])bsh.get("this.methods")) {
-                candidates.add(new Candidate(AttributedString.stripAnsi(v) + "(", v, null, null, null, null, true));
-            }
-            
-            String completes = (String)bsh.get("COMPLETES");
-            if (completes != null) {
-                for (String v : completes.split(",")) {
-                    v = v.trim();
-                    candidates.add(new Candidate(AttributedString.stripAnsi(v), v, null, null, null, null, true));
-                }
-            }
-            
-        } catch (EvalError x) {
-            //
-            // it should not happen...
-            //
-        }
+        for (String c : names.completeName(pl.line()))
+            candidates.add(new Candidate(c));
+        for (String c: findMembers(pl.line().replaceFirst("\\.[^\\.]*$","")))
+            candidates.add(new Candidate(c));
     }
     
 }
