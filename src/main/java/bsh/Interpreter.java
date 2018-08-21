1e72f2538b3f58df6075cbcbb4828ea2744d9c62
Stefano Fornari <stefano.fornari@gmail.com>
2018-08-21 22:01:03 +0200

    moved pathToFile into a command

--- src/main/java/bsh/Interpreter.java
+++ src/main/java/bsh/Interpreter.java
@@ -731,28 +731,6 @@ public class Interpreter
         } catch (InterruptedException e1) { /* ignore */ }
     }
 
-    /**
-        Localize a path to the file name based on the bsh.cwd interpreter
-        working directory.
-
-        TODO: to me moved into a command
-    */
-    public File pathToFile( String fileName )
-        throws IOException
-    {
-        File file = new File( fileName );
-
-        // if relative, fix up to bsh.cwd
-        if ( !file.isAbsolute() ) {
-            String cwd = (String)getu("bsh.cwd");
-            file = new File( cwd + File.separator + fileName );
-        }
-
-        // The canonical file name is also absolute.
-        // No need for getAbsolutePath() here...
-        return new File( file.getCanonicalPath() );
-    }
-
     public static void redirectOutputToFile( String filename )
     {
         try {
